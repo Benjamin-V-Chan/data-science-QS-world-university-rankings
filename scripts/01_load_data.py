8ec7466b486008bfde6ccb05c21e78f2a85e8b98
BenjaminChan <benjamin.v.chan@gmail.com>
2025-04-21 23:10:09 -0700

    01_load_data script outline

---
+++ scripts/01_load_data.py
@@ -0,0 +1,7 @@
+# - import pandas
+# - define load_data(path):
+#     read CSV at path into DataFrame
+#     return DataFrame
+# - in main:
+#     call load_data on 'data/raw/qs_rankings_2025.csv'
+#     save raw DataFrame to 'data/processed/raw_rankings.csv'
